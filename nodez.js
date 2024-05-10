84e379552ffbd5b80574a34ea0327be3ebfc4517
AnaPislaru <149891406+AnaPislaru@users.noreply.github.com>
2024-05-10 14:55:07 +0300

    Update nodez.js

--- nodez.js
+++ nodez.js
@@ -1,28 +1,45 @@
 (function () {
+
     // create dialog elements
+
     const dialog = document.createElement("ui5-dialog");
+
     const content = document.createElement("div");
+
     const footer = document.createElement("footer");
+
     const footerBtn = document.createElement("ui5-button");
+
     // set attributes
-    dialog.setAttribute("header-text", "Third Party JavaScript");
-	dialog.innerHTML='<object type="text/html" data="www.google.com" ></object>';
-//    footer.setAttribute("slot", "footer");
+
+    dialog.setAttribute("header-text", "Ana java script");
+
+    footer.setAttribute("slot", "footer");
+
     // content
-//content.innerHTML = "This dialog was created by third party JS file";
+
+    content.innerHTML = "This dialog was created by third party JS file";
+
     content.style.padding = "1rem";
+
     // footer btn
+
     footerBtn.onclick = () => dialog.close();
+
     footerBtn.innerHTML = "Close";
+
     // append elements
+
     dialog.appendChild(content);
+
     dialog.appendChild(footer);
+
     footer.appendChild(footerBtn);
+
     document.body.appendChild(dialog);
+
     // open dialog
+
     dialog.show();
-	
-//	function load_home() {
-//     document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
-//}
+
 })();
